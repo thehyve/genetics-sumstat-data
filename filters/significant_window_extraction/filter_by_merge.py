347fa198c8e6df74b4edafcd8cb58f015bd67605
Roman Hillje <roman.hillje@googlemail.com>
2021-08-18 17:50:45 +0200

    Rename column "type" to "type_id"
    This is required for the `sa/gwas` data type.

--- filters/significant_window_extraction/filter_by_merge.py
+++ filters/significant_window_extraction/filter_by_merge.py
@@ -98,7 +98,9 @@ def filter_significant_windows(in_pq, out_pq, data_type, window, pval):
 
     # Repartition
     merged = (
-        merged.repartitionByRange('chrom', 'pos')
+        merged
+        .withColumnRenamed('type', 'type_id')
+        .repartitionByRange('chrom', 'pos')
         .sortWithinPartitions('chrom', 'pos')
     )
 
